3a81350ba0f5b7bca1308147a56d80ae0ebe0717
Charlotte Moore <mooc123@gmail.com>
2016-12-12 19:36:50 -0700

    added two more test to test functionality of trie

--- test/trie_test.rb
+++ test/trie_test.rb
@@ -42,11 +42,29 @@ class TrieTest < MiniTest::Test
     end
 
     def test_insert_can_create_nodes_for_five_letter_word
-
       trie = Trie.new
       trie.insert_words("pizza")
-      binding.pry
-      # assert_equal
+      # binding.pry
 
+      assert_equal trie.root.children.keys, ["p"]
+      assert_equal trie.root.children["p"].children.keys, ["i"]
+      assert_equal trie.root.children["p"].children["i"].children["z"].children["z"].children.keys, ["a"]
+    end
+
+    def test_insert_can_insert_two_words_with_same_starting_letter
+      trie = Trie.new
+      trie.insert_words("hat")
+      trie.insert_words("ham")
+
+      assert_equal trie.root.children["h"].children.keys, ["a"]
+      assert_equal trie.root.children["h"].children["a"].children.keys, ["t", "m"]
+    end
+
+    def test_insert_can_insert_two_words_with_different_starting_letters
+      trie = Trie.new
+      trie.insert_words("hat")
+      trie.insert_words("cat")
+
+      assert_equal trie.root.children.keys, ["h", "c"]
     end
 end
